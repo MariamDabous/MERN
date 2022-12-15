ef51191a628b6582b66e74ee2a41bf89fde23846
MariamDabous <mariam.dabous@hotmail.com>
2022-12-15 16:03:21 +0200

    test1

---
+++ React/first-jsx/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>Learn react </li>
+        <li>Climb Everest </li>
+        <li>Run marathon </li>
+        <li>Feed the dogs </li>
+      </ul>
+      
+    </div>
+  );
+}
+
+export default App;
